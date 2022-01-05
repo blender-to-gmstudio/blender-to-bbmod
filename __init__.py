9188f4a06339caca812bbb17a29653d40bb76bc8
Bart Teunis <bart_teunis@hotmail.com>
2022-01-05 23:27:43 +0100

    Reworked the code a bit to prepare for BBMOD v3

--- __init__.py
+++ __init__.py
@@ -3,62 +3,64 @@ bl_info = {
     "description": "Export to BBMOD",
     "author": "Bart Teunis",
     "version": (0, 1, 0),
-    "blender": (2, 82, 0),
+    "blender": (3, 0, 0),
     "location": "File > Export",
     "warning": "", # used for warning icon and text in addons panel
-    "wiki_url": "",
+    "doc_url": "",
     "category": "Import-Export"}
 
 import bpy
+from mathutils import Matrix
 from struct import pack
 
-BBMOD_header_string = "bbmod\0"
+BBMOD_version = 3
+
+vertex_attributes = [
+    'vertices',
+    'normals',
+    'texcoords',
+    'colors',
+    'tangentw',
+    'bones',
+    'ids',
+]
 
 def matrix_flatten(matrix):
     val = []
-    for row in context.object.matrix_world:
+    for row in matrix:
         val.extend(row[:])
     return val
 
-def write_bbmod(context, filepath, use_some_setting):
+def write_bbmod(context, filepath, use_some_setting, vertex_format):
     object_list = context.selected_objects
-    model_list = [o for o in object_list if o.type=='MESH']
-    
+    model_list = [object for object in object_list if object.type=='MESH']
+    # TODO "if object in 'meshlike' types"
+
     # Type and version
-    header_bytes = bytearray("bbmod\0", 'utf-8')# BBMOD identifier
-    header_bytes.extend(pack('B', 2))           # BBMOD version
-    
-    # Write vertex format (currently everything in it)
+    header_bytes = bytearray("bbmod\0", 'utf-8')    # BBMOD identifier
+    header_bytes.extend(pack('B', BBMOD_version))   # BBMOD version
+
+    # Write vertex format
+    format_bools = [attrib in vertex_format for attrib in vertex_attributes]
     vertex_format_bytes = bytearray()
-    vertex_format_bytes.extend(pack('?', True)) # Vertices
-    vertex_format_bytes.extend(pack('?', True)) # Normals
-    vertex_format_bytes.extend(pack('?', True)) # TextureCoords
-    vertex_format_bytes.extend(pack('?', True)) # Colors
-    vertex_format_bytes.extend(pack('?', True)) # TangentW
-    vertex_format_bytes.extend(pack('?', True)) # Bones
-    vertex_format_bytes.extend(pack('?', True)) # Ids
-    
+    vertex_format_bytes.extend(pack('?' * len(format_bools), *format_bools))
+
     # Write meshes
     meshes_bytes = bytearray()
     meshes_bytes.extend(pack('I', len(model_list)))
-    for mesh in model_list:
+    for mesh_object in model_list:
+        mesh = mesh_object.data
         meshes_bytes.extend(pack('I', len(mesh.polygons) * 3))
+
         # TODO Write vertex buffer data using the format provided
-        
+
         meshes_bytes.extend(pack('I', 0))       # Material index
-        
-        
-    
-    # Global inverse transform matrix
-    matrix_bytes = bytearray()
-    values = matrix_flatten(context.object.matrix_world)
-    matrix_bytes.extend(pack('f' * 16, *values))
-    
+
     # Node count and root node
     node_name = bytearray("RootNode" + "\0", 'utf-8')
     node_matrix = Matrix()
     values = matrix_flatten(node_matrix)
-    
+
     node_bytes = bytearray()
     node_bytes.extend(pack('I', 1))             # NodeCount
     node_bytes.extend(node_name)
@@ -66,32 +68,31 @@ def write_bbmod(context, filepath, use_some_setting):
     node_bytes.extend(pack('?', True))          # IsBone
     node_bytes.extend(pack('?', True))          # Visible
     node_bytes.extend(pack('f' * 16, *values))  # Matrix
-    
+
     node_bytes.extend(pack('I', len(model_list)))# Mesh count
-    
+
     node_bytes.extend(pack('I', 0))             # Mesh indices
-    
+
     node_bytes.extend(pack('I', 0))             # Child count
-    
+
     # Skeleton
     skeleton_bytes = bytearray()
     skeleton_bytes.extend(pack('I', 0))
-    
+
     # Materials
     material_bytes = bytearray()
     material_bytes.extend(pack('I', 1))         # MaterialCount
     material_bytes.extend(bytearray("DefaultMaterial" + "\0", 'utf-8'))
-    
+
     # Write everything to file
     with open(filepath, "wb") as file:
         file.write(header_bytes)
         file.write(vertex_format_bytes)
         file.write(meshes_bytes)
-        file.write(matrix_bytes)
         file.write(node_bytes)
         file.write(skeleton_bytes)
         file.write(material_bytes)
-    
+
     return {'FINISHED'}
 
 
@@ -116,26 +117,30 @@ class ExportBBMOD(Operator, ExportHelper):
         maxlen=255,  # Max internal buffer length, longer would be clamped.
     )
 
-    # List of operator properties, the attributes will be assigned
-    # to the class instance from the operator settings before calling.
     use_setting: BoolProperty(
         name="Example Boolean",
         description="Example Tooltip",
         default=True,
     )
 
-    type: EnumProperty(
-        name="Example Enum",
+    vertex_format: EnumProperty(
+        name="Vertex Format",
         description="Choose between two items",
         items=(
-            ('OPT_A', "First Option", "Description one"),
-            ('OPT_B', "Second Option", "Description two"),
+            ('vertices', "Vertices", "Whether the vertex format has vertices"),
+            ('normals', "Normals", "Whether the vertex format has normals"),
+            ('texcoords', "UVs", "Whether the vertex format has uvs"),
+            ('colors', "Colors", "Whether the vertex format has vertex colors"),
+            ('tangentw', "TangentW", "Whether the vertex format has tangent and bitangent sign"),
+            ('bones', "Bones", "Whether the vertex format has vertex weights and bone indices"),
+            ('ids', "Ids", "Whether the vertex format has ids for dynamic batching"),
         ),
-        default='OPT_A',
+        default={'vertices'},
+        options={'ENUM_FLAG'},
     )
 
     def execute(self, context):
-        return write_bbmod(context, self.filepath, self.use_setting)
+        return write_bbmod(context, self.filepath, self.use_setting, self.vertex_format)
 
 
 # Only needed if you want to add into a dynamic menu
